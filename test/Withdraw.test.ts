e68371044c3453c2136ed709d7c815a33084db41
literandltx <romanskovronn@gmail.com>
2024-09-13 12:24:22 +0300

    Add transfer metrics method collection as test

--- test/Withdraw.test.ts
+++ test/Withdraw.test.ts
@@ -1,18 +1,13 @@
 import { ethers } from "hardhat";
 import { Depositor } from "@ethers-v6";
-import { BytesLike } from "ethers";
 import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
 
 import { expect } from "chai";
 
-import {
-  getPoseidon,
-  getCommitment,
-  generateSecrets,
-  Reverter,
-  getBytes32PoseidonHash,
-  getZKP, CommitmentFields
-} from "@test-helpers";
+import { CommitmentFields, generateSecrets, getCommitment, getPoseidon, getZKP, Reverter } from "@test-helpers";
+import * as fs from "node:fs";
+
+const treeHeight = 32;
 
 describe("Withdraw", () => {
   const reverter = new Reverter();
@@ -21,8 +16,6 @@ describe("Withdraw", () => {
   let USER1: SignerWithAddress;
 
   let depositor: Depositor;
-
-  let treeHeight = 6;
 
   before(async () => {
     [OWNER, USER1] = await ethers.getSigners();
@@ -161,8 +154,6 @@ describe("Transfer", () => {
 
   let depositor: Depositor;
 
-  let treeHeight = 6;
-
   before(async () => {
     [OWNER, USER1, USER2] = await ethers.getSigners();
 
@@ -214,5 +205,144 @@ describe("Transfer", () => {
 
     // A: transfer deposit
     await depositor.transfer(proof.nullifierHash, commitmentB, root, proof.formattedProof);
+  })
+
+  function calculateExpectation(arr: number[]): number {
+    if (arr.length === 0) {
+      throw new Error("Array is empty. Cannot calculate expectation.");
+    }
+
+    return arr.reduce((acc, curr) => acc + curr, Number(0)) / Number(arr.length);
+  }
+
+  async function sum(arr: number[]): Promise<number> {
+    return arr.reduce((acc, curr) => acc + curr, Number(0));
+  }
+
+  async function singleBunchTransfer(bunch: number): Promise<{ gasUsedSum: number; timeSpentSum: number }> {
+    const eth_value = "1";
+    const gasUsed: number[] = [];
+    const timeAmount: number[] = [];
+
+    for (let i = 0; i < bunch; i++) {
+      const pairA: CommitmentFields = generateSecrets();
+      const commitmentA = getCommitment(pairA);
+      await depositor.deposit(commitmentA as any, { value: ethers.parseEther(eth_value) } as any);
+
+      const pairB: CommitmentFields = generateSecrets();
+      const commitmentB = getCommitment(pairB);
+
+      const proof = await getZKP(depositor, pairA, commitmentB);
+
+      const start = performance.now();
+
+      const txResponse = await depositor.transfer(proof.nullifierHash, commitmentB, await depositor.getRoot(), proof.formattedProof);
+      const txReceipt = await txResponse.wait();
+      const gas = txReceipt?.gasUsed;
+
+      const end = performance.now();
+
+      if (gas) {
+        gasUsed.push(Number(gas));
+      }
+      timeAmount.push(end - start);
+    }
+
+    const gasUsedSum = await sum(gasUsed);
+    const timeSpentSum = await sum(timeAmount);
+
+    return {gasUsedSum, timeSpentSum};
+  }
+
+  function findMaxMinAndIndexes(arr: number[]): { max: { value: number, index: number }, min: { value: number, index: number } } {
+    if (arr.length === 0) {
+      throw new Error('Array cannot be empty');
+    }
+
+    let max = arr[0];
+    let min = arr[0];
+    let maxIndex = 0;
+    let minIndex = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+        maxIndex = i;
+      }
+      if (arr[i] < min) {
+        min = arr[i];
+        minIndex = i;
+      }
+    }
+
+    return {
+      max: { value: max, index: maxIndex },
+      min: { value: min, index: minIndex }
+    };
+  }
+
+  async function transferMetrics(bunch: number, counter: number): Promise<{gasExpectation: number[], timeExpectation: number[]}> {
+    const gasExpectation: number[] = [];
+    const timeExpectation: number[] = [];
+
+    for (let i = 0; i < counter; i++) {
+      const result = await singleBunchTransfer(bunch);
+      const expectation1 = result.gasUsedSum;
+      const expectation2 = result.timeSpentSum;
+
+      console.log(`Gas simple bunch:  ${expectation1.toString()}`);
+      console.log(`Time simple bunch: ${expectation2.toString()}\n`);
+
+      gasExpectation.push(expectation1);
+      timeExpectation.push(expectation2);
+    }
+
+    return {gasExpectation, timeExpectation}
+  }
+
+  function saveResults(gasExpectation: number[], timeExpectation: number[], bunch: number, count: number): void {
+    try {
+      const data = {
+        gasExpectation,
+        timeExpectation
+      };
+
+      const jsonData = JSON.stringify(data, null, 2);
+
+      const date = new Date();
+      const timestamp = date.getDay().toString() + "_" + date.getHours().toString() + "_" + date.getMinutes().toString();
+      const filename = `${bunch}_${count}_results_${timestamp}.json`;
+
+      fs.writeFileSync(filename, jsonData);
+
+      console.log(`Results saved to ${filename}`);
+    } catch (error) {
+      console.error('Error saving results:', error);
+    }
+  }
+
+  it.only("transfer (bunch) metrics", async () => {
+    const bunch = 10;
+    const count = 10;
+    const result = await transferMetrics(bunch, count);
+    console.log("All transactions are complete");
+
+    const gasExpectation = calculateExpectation(result.gasExpectation);
+    const timeExpectation = calculateExpectation(result.timeExpectation);
+
+    console.log('\nfinal results')
+    console.log(`Gas expectation:  ${gasExpectation.toString()}`);
+    console.log(`Time expectation: ${timeExpectation.toString()}`);
+
+    saveResults(result.gasExpectation, result.timeExpectation, bunch, count);
+
+    const gasRange = findMaxMinAndIndexes(result.gasExpectation);
+    const timeRange = findMaxMinAndIndexes(result.timeExpectation);
+
+    console.log('Max gas value: ', gasRange.max.value, 'at index:', gasRange.max.index);
+    console.log('Min gas value: ', gasRange.min.value, 'at index:', gasRange.min.index);
+
+    console.log('Max time value:', timeRange.max.value, 'at index:', timeRange.max.index);
+    console.log('Min time value:', timeRange.min.value, 'at index:', timeRange.min.index);
   })
 })
