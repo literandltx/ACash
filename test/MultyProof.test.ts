3354dbde624e797af6035ae4ea2a18b64b3a69be
literandltx <romanskovronn@gmail.com>
2024-09-25 18:05:04 +0300

    Reorganize tests structure. Delete redundant comments. Add new test cases as random inputs. Add few test mocks. Fix next hash pair creation. Delete redundant function.

--- test/MultyProof.test.ts
+++ test/MultyProof.test.ts
@@ -45,92 +45,144 @@ describe.only("MultiProof", () => {
 
   afterEach(reverter.revert);
 
-  /* one empty node and two none empty node scenario
-  [
-    {
-      secret: '0x00000000551e9caba346265ab896e145a3d3d6359dad756b2b998a5fe1e82b6b',
-      nullifier: '0x000000008660e62918c45569bfc87a180019d029f8257b662d8797f0a4b2e443'
-    },
-    {
-      secret: '0x000000001a51692da01c96034e670c471bffa3899e3b452ac6669abdc57dffe4',
-      nullifier: '0x00000000aa3eb6b01d78d2dc512621301a62ca845066703b3396e94d32659797'
-    }
-  ]
-   */
+  describe("Tree with two leaf at level 0 only", () => {
+    it("should gen multiproof for two pairs, case 1", async () => {
+      /**
+       * root
+       * ├── p1
+       * └── p2
+       */
+      const pairs: CommitmentFields[] =
+        [
+          {
+            secret: '0x00000000572c05630baa29dbb30a8d8c96a362b054b443d3155354d282d23f69',
+            nullifier: '0x0000000014c47f1bbf7418d8798264a9c4073b175f1e54b5da0890778c14eb22'
+          },
+          {
+            secret: '0x00000000919249b48088b75045915bfb71d83f96f64157f41f3d5eaffd2171fc',
+            nullifier: '0x00000000b4b632b1e490488938c5ac2352cc5dc7d6e7639fe0dd34be808466ef'
+          }
+        ];
+      await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
 
-  /* like previous but 3 leafs
-  [
-    {
-      secret: '0x00000000f1ce6c627c1dd83599842a08899acb929480d39be8d98b1f10f1c20f',
-      nullifier: '0x00000000bf4ce6f519179b3835a9b81e662c696e46fa376a7dba6544ab6eb8f1'
-    },
-    {
-      secret: '0x000000007cbc88e3ad8ded8749a70eac76735f42b3d16662bde0d15c6c09cf5b',
-      nullifier: '0x0000000011ff27260e1d055a97ae3b69a5dc717fef1718590055d5a75711d697'
-    },
-    {
-      secret: '0x00000000ed1b4bbd5565a99005e07e6f26444fefe26a4e0375bb552a22df2c1f',
-      nullifier: '0x00000000eab2105c1da1be89554f8aff0ae9093de565b67c799129f8768b089a'
-    }
-  ]
-   */
+      const smtMultiProof = await getMultiProof(depositor, pairs);
 
-  it("should gen multiproof for two pairs, only two leaf in tree scenario 1", async () => {
-    /**
-     * root
-     * ├── p1
-     * └── p2
-     */
-    const pairs: CommitmentFields[] =
-      [
-        {
-          secret: '0x00000000572c05630baa29dbb30a8d8c96a362b054b443d3155354d282d23f69',
-          nullifier: '0x0000000014c47f1bbf7418d8798264a9c4073b175f1e54b5da0890778c14eb22'
-        },
-        {
-          secret: '0x00000000919249b48088b75045915bfb71d83f96f64157f41f3d5eaffd2171fc',
-          nullifier: '0x00000000b4b632b1e490488938c5ac2352cc5dc7d6e7639fe0dd34be808466ef'
-        }
-      ];
-    await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
+      expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
+      expect(smtMultiProof.proof.length).to.equal(0);
+      expect(smtMultiProof.proof).to.deep.be.equal([]);
+      expect(smtMultiProof.pairs).to.deep.be.equal(pairs);
+    });
 
-    const smtMultiProof = await getMultiProof(depositor, pairs);
+    it("should gen multiproof for two pairs, case 2", async () => {
+      /**
+       * root
+       * ├── p1
+       * └── p2
+       */
+      const pairs: CommitmentFields[] =
+        [
+          {
+            secret: '0x000000004ea5ae2fc9bfa24b0b4e3c43d2d11f18404b69efe35477c6f86ab899',
+            nullifier: '0x000000005be9fbfdddd4197dcfc7235e77aa1daf72008f9ac47997eded10e443'
+          },
+          {
+            secret: '0x00000000e03d579dbf882389e607163de243ba67e24eb5c4485679a972e8c3a5',
+            nullifier: '0x0000000081682627e20754f21b8449b665ba984728ce7af08b5a674ac24be959'
+          }
+        ];
+      await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
 
-    expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
-    expect(smtMultiProof.proof.length).to.equal(0);
-    expect(smtMultiProof.proof).to.deep.be.equal([]);
-    expect(smtMultiProof.pairs).to.deep.be.equal(pairs);
-  });
+      const smtMultiProof = await getMultiProof(depositor, pairs);
 
-  // not works
-  it("should gen multiproof for two pairs, only two leaf in tree scenario 2", async () => {
-    /**
-     * root
-     * ├── p1
-     * └── p2
-     */
-    const pairs: CommitmentFields[] =
-      [
-        {
-          secret: '0x000000004ea5ae2fc9bfa24b0b4e3c43d2d11f18404b69efe35477c6f86ab899',
-          nullifier: '0x000000005be9fbfdddd4197dcfc7235e77aa1daf72008f9ac47997eded10e443'
-        },
-        {
-          secret: '0x00000000e03d579dbf882389e607163de243ba67e24eb5c4485679a972e8c3a5',
-          nullifier: '0x0000000081682627e20754f21b8449b665ba984728ce7af08b5a674ac24be959'
-        }
-      ];
-    await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
+      expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
+      expect(smtMultiProof.proof.length).to.equal(0);
+      expect(smtMultiProof.proof).to.deep.be.equal([]);
+      expect(smtMultiProof.pairs).to.deep.be.equal(pairs);
+    });
 
-    const smtMultiProof = await getMultiProof(depositor, pairs);
+    it("should gen multiproof for two pairs, case 3", async () => {
+      /**
+       * root
+       * ├── p1
+       * └── p2
+       */
+      const pairs: CommitmentFields[] =
+        [
+          {
+            secret: '0x0000000028d4bf96ed3abb2a23a8cc77ddba658e6d6e9c18c758d893d987db26',
+            nullifier: '0x000000004e3c8a30c6e38f2fd359adb64084280ac7dbd63cd51a34a03015c92b'
+          },
+          {
+            secret: '0x000000001519714685473470bc576e12dfa3819bb3ebd6405b590a242a6a618b',
+            nullifier: '0x00000000712b55938a7db64c6d06a01899446c9aa474866550a972a926c0c4b5'
+          }
+        ];
+      await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
 
-    expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
-    expect(smtMultiProof.proof.length).to.equal(0);
-    expect(smtMultiProof.proof).to.deep.be.equal([]);
-    expect(smtMultiProof.pairs).to.deep.be.equal(pairs);
-  });
+      const smtMultiProof = await getMultiProof(depositor, pairs);
 
-  describe("tree with 3 leaf, without empty nodes", async () => {
+      expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
+      expect(smtMultiProof.proof.length).to.equal(0);
+      expect(smtMultiProof.proof).to.deep.be.equal([]);
+      expect(smtMultiProof.pairs).to.deep.be.equal(pairs);
+    });
+
+    it("should gen multiproof for two pairs, case 4", async () => {
+      /**
+       * root
+       * ├── p1
+       * └── p2
+       */
+      const pairs: CommitmentFields[] =
+        [
+          {
+            secret: '0x0000000032848c856dbbf0523edc821a414f4a4ca39377207e4c3116cf76e9e3',
+            nullifier: '0x00000000f53aab3b94e2aa94f400e3c931583e5b1d233721e08d35f83c189fb4'
+          },
+          {
+            secret: '0x00000000abc7822e35461ecbdc747da6c1a258331124cd787bda929d61dc418c',
+            nullifier: '0x00000000ec5e300a2df03788ebf43b2e45e8d46600bf398404cce4d82c6a57ef'
+          }
+        ];
+      await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
+
+      const smtMultiProof = await getMultiProof(depositor, pairs);
+
+      expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
+      expect(smtMultiProof.proof.length).to.equal(0);
+      expect(smtMultiProof.proof).to.deep.be.equal([]);
+      expect(smtMultiProof.pairs).to.deep.be.equal(pairs);
+    });
+
+    it("should gen multiproof for two pairs, case 5", async () => {
+      /**
+       * root
+       * ├── p1
+       * └── p2
+       */
+      const pairs: CommitmentFields[] =
+        [
+          {
+            secret: '0x000000001e125eaf6d6d0973773098c314e2d47dcb0bd78c8ed1844a84149e86',
+            nullifier: '0x00000000a1ed6ada4aa61f528889db4650973d3fe20bb0ca20fcdfe8e787f528'
+          },
+          {
+            secret: '0x00000000cef64361b85a2bff8ef721237ba895b8c65fa32d4372af92925fb4a7',
+            nullifier: '0x0000000056318e6bd4c64a57892b251c1c554552ba8c65dbced4d602ab504a1a'
+          }
+        ];
+      await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
+
+      const smtMultiProof = await getMultiProof(depositor, pairs);
+
+      expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
+      expect(smtMultiProof.proof.length).to.equal(0);
+      expect(smtMultiProof.proof).to.deep.be.equal([]);
+      expect(smtMultiProof.pairs).to.deep.be.equal(pairs);
+    });
+  })
+
+  describe("Tree with 3 leaf without empty nodes", () => {
     /**
      * root
      * ├── p2
@@ -249,29 +301,138 @@ describe.only("MultiProof", () => {
       expect(smtMultiProof.proof.length).to.equal(0);
       expect(smtMultiProof.pairs).to.deep.be.equal(pairs);
     });
+
+    Array.from({ length: 10 }, () => {
+      it("should gen multiproof for 1/2/3 pairs (random)", async () => {
+        const randomNumber = Math.floor(Math.random() * 3) + 1;
+        await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
+        const chosenPairs = pickRandomElements(pairs, randomNumber);
+
+        const smtMultiProof = await getMultiProof(depositor, chosenPairs);
+
+        expect(smtMultiProof.pairs).to.deep.be.equal(chosenPairs);
+        expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
+      });
+    })
+  })
+
+  // todo
+  describe("Tree with 2 leaf node node and 1 zero node at level 0", () => {
+    /**
+     * root
+     * ├── 0
+     * └── H1
+     *     ├── p1
+     *     └── p2
+     */
+    const pairs: CommitmentFields[] =
+      [
+        {
+          secret: '0x00000000551e9caba346265ab896e145a3d3d6359dad756b2b998a5fe1e82b6b',
+          nullifier: '0x000000008660e62918c45569bfc87a180019d029f8257b662d8797f0a4b2e443'
+        },
+        {
+          secret: '0x000000001a51692da01c96034e670c471bffa3899e3b452ac6669abdc57dffe4',
+          nullifier: '0x00000000aa3eb6b01d78d2dc512621301a62ca845066703b3396e94d32659797'
+        }
+      ];
+  })
+
+  // todo
+  describe("Tree with 3 leaf node node and 1 zero node at level 0", () => {
+    /**
+     * root
+     * ├── 0
+     * └── H1
+     *     ├── p1
+     *     └── H2
+     *         ├── p2
+     *         └── p3
+     */
+    const pairs: CommitmentFields[] = [
+      {
+        secret: '0x00000000f1ce6c627c1dd83599842a08899acb929480d39be8d98b1f10f1c20f',
+        nullifier: '0x00000000bf4ce6f519179b3835a9b81e662c696e46fa376a7dba6544ab6eb8f1'
+      },
+      {
+        secret: '0x000000007cbc88e3ad8ded8749a70eac76735f42b3d16662bde0d15c6c09cf5b',
+        nullifier: '0x0000000011ff27260e1d055a97ae3b69a5dc717fef1718590055d5a75711d697'
+      },
+      {
+        secret: '0x00000000ed1b4bbd5565a99005e07e6f26444fefe26a4e0375bb552a22df2c1f',
+        nullifier: '0x00000000eab2105c1da1be89554f8aff0ae9093de565b67c799129f8768b089a'
+      }
+    ];
+  })
+
+  // todo
+  describe.skip("Tree with large random input", () => {
+    Array.from({ length: 5 }, () => {
+      it.skip("should gen proof for 5 random leaf from 50", async () => {
+        const pairNumberToGen = 50;
+        const pairNumberToProof = 5;
+        const pairs: CommitmentFields[] = Array.from({ length: pairNumberToGen }, () => generateSecrets());
+
+        await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
+        const chosenPairs = pickRandomElements(pairs, pairNumberToProof);
+
+        const smtMultiProof = await getMultiProof(depositor, chosenPairs);
+
+        expect(smtMultiProof.pairs).to.deep.be.equal(chosenPairs);
+        expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
+      });
+    });
+
+    Array.from({ length: 5 }, () => {
+      it.skip("should gen proof for 5 random leaf from 125", async () => {
+
+      });
+    });
+
+    Array.from({ length: 2 }, () => {
+      it.skip("should gen proof for 5 random leaf from 300", async () => {
+
+      });
+    });
+
+    Array.from({ length: 1 }, () => {
+      it.skip("should gen proof for 5 random leaf from 1200", async () => {
+        const pairNumberToGen = 1200;
+        const pairNumberToProof = 5;
+        const pairs: CommitmentFields[] = Array.from({ length: pairNumberToGen }, () => generateSecrets());
+        await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
+
+        const smtMultiProof = await getMultiProof(depositor, pickRandomElements(pairs, pairNumberToProof));
+
+        expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
+        expect(smtMultiProof.proof.length).to.equal(0);
+        expect(smtMultiProof.proof).to.deep.be.equal([]);
+        expect(smtMultiProof.pairs).to.deep.be.equal(pairs);
+      });
+    });
   })
 
   it("should test for gen test cases", async () => {
     const pairs: CommitmentFields[] =
       [
         {
-          secret: '0x000000004ea5ae2fc9bfa24b0b4e3c43d2d11f18404b69efe35477c6f86ab899',
-          nullifier: '0x000000005be9fbfdddd4197dcfc7235e77aa1daf72008f9ac47997eded10e443'
+          secret: '0x00000000f1ce6c627c1dd83599842a08899acb929480d39be8d98b1f10f1c20f',
+          nullifier: '0x00000000bf4ce6f519179b3835a9b81e662c696e46fa376a7dba6544ab6eb8f1'
         },
         {
-          secret: '0x00000000e03d579dbf882389e607163de243ba67e24eb5c4485679a972e8c3a5',
-          nullifier: '0x0000000081682627e20754f21b8449b665ba984728ce7af08b5a674ac24be959'
+          secret: '0x000000007cbc88e3ad8ded8749a70eac76735f42b3d16662bde0d15c6c09cf5b',
+          nullifier: '0x0000000011ff27260e1d055a97ae3b69a5dc717fef1718590055d5a75711d697'
+        },
+        {
+          secret: '0x00000000ed1b4bbd5565a99005e07e6f26444fefe26a4e0375bb552a22df2c1f',
+          nullifier: '0x00000000eab2105c1da1be89554f8aff0ae9093de565b67c799129f8768b089a'
         }
       ];
-    const numberToGen   = 2;
-    const numberToProof = 2;
-    // const pairs: CommitmentFields[] = Array.from({ length: numberToGen }, () => generateSecrets());
     console.log("Contract: ");
     await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
     console.log("\nTS: ");
-    // console.log(pairs);
 
-    const smtMultiProof = await getMultiProof(depositor, pickRandomElements(pairs, numberToProof));
+    const smtMultiProof = await getMultiProof(depositor, [pairs[0], pairs[1]]);
 
     console.log("\nResults: ");
     console.log(BigInt(await depositor.getRoot()) + " root expected");
@@ -279,7 +440,11 @@ describe.only("MultiProof", () => {
     console.log(smtMultiProof.root === BigInt(await depositor.getRoot()));
   });
 
-  async function getMultiProof(contract: Depositor, pairs: CommitmentFields[]): Promise<{ pairs: CommitmentFields[], proof: bigint[], root: bigint }> {
+  async function getMultiProof(contract: Depositor, pairs: CommitmentFields[]): Promise<{
+    pairs: CommitmentFields[],
+    proof: bigint[],
+    root: bigint
+  }> {
     type NodeInfo = {
       currentNodeHash: bigint
       currentNodeKey: bigint
@@ -308,19 +473,6 @@ describe.only("MultiProof", () => {
       });
     }
 
-    // console.log();
-    // console.log("key");
-    nodeInfos.forEach(node => {
-      // console.log(node.currentNodeKey);
-      // console.log(node.siblings);
-      // console.log(node.siblings.map(num => BigInt(num)));
-      // console.log(node.siblingIndex);
-    })
-    console.log();
-
-    // console.log(BigInt(nodeInfos[0].siblings[0]));
-    // console.log(BigInt(nodeInfos[1].siblings[0]));
-
     while (nodeInfos[0].siblingIndex !== -1) {
       const maxIndex = Math.max(...nodeInfos.map(node => node.siblingIndex));
       const B: bigint[][] = [];
@@ -330,15 +482,11 @@ describe.only("MultiProof", () => {
         if (node.siblingIndex === maxIndex) {
           let pairNextNodeHash: bigint[];
 
-          // console.log(node.currentNodeHash);
-
-          if (isRight(node.currentNodeHash, node.siblingIndex)) {
+          if (isRight(node.currentNodeKey, node.siblingIndex)) {
             pairNextNodeHash = [BigInt(node.siblings[node.siblingIndex]), node.currentNodeHash];
           } else {
             pairNextNodeHash = [node.currentNodeHash, BigInt(node.siblings[node.siblingIndex])];
           }
-
-          // console.log(pairNextNodeHash);
 
           const elementExists = B.some(existingElement => existingElement[0] === pairNextNodeHash[0] && existingElement[1] === pairNextNodeHash[1]);
 
@@ -381,10 +529,6 @@ describe.only("MultiProof", () => {
     return 0;
   }
 
-  function toHex(bigInt: BigInt): string {
-    return bigInt.toString(16).padStart(64, ' ');
-  }
-
   async function proceedCertainDeposit(pair: CommitmentFields) {
     await depositor.deposit(getCommitment(pair) as any, { value: ethers.parseEther(eth_value) } as any);
   }
@@ -404,14 +548,6 @@ describe.only("MultiProof", () => {
 
     const shuffled = arr.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, count);
-  }
-
-  function swapIfZero(arr: [bigint, bigint]): [bigint, bigint] {
-    if (arr[0] === 0n || arr[1] === 0n) {
-      return [arr[1], arr[0]];
-    }
-
-    return arr;
   }
 
 });
