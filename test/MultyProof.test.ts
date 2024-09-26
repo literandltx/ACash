f80c80cdaff8669d44dc2ae454b000b4f5362ae5
literandltx <romanskovronn@gmail.com>
2024-09-26 14:00:26 +0300

    Add new test cases where it fails. Prepare env for debug

--- test/MultyProof.test.ts
+++ test/MultyProof.test.ts
@@ -395,7 +395,6 @@ describe.only("MultiProof", () => {
     });
   })
 
-  // todo
   describe("Tree with 3 leaf node node and 1 zero node at level 0", () => {
     /**
      * root
@@ -420,6 +419,76 @@ describe.only("MultiProof", () => {
         nullifier: '0x00000000eab2105c1da1be89554f8aff0ae9093de565b67c799129f8768b089a'
       }
     ];
+
+    it("should gen multiproof for 1 pair [0]", async () => {
+      const pairToProof: CommitmentFields[] = [pairs[0]];
+      await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
+
+      const smtMultiProof = await getMultiProof(depositor, pairToProof);
+
+      expect(smtMultiProof.pairs).to.deep.equal(pairToProof);
+      expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
+    });
+
+    it("should gen multiproof for 1 pair [1]", async () => {
+      const pairToProof: CommitmentFields[] = [pairs[1]];
+      await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
+
+      const smtMultiProof = await getMultiProof(depositor, pairToProof);
+
+      expect(smtMultiProof.pairs).to.deep.equal(pairToProof);
+      expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
+    });
+
+    it("should gen multiproof for 1 pair [2]", async () => {
+      const pairToProof: CommitmentFields[] = [pairs[2]];
+      await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
+
+      const smtMultiProof = await getMultiProof(depositor, pairToProof);
+
+      expect(smtMultiProof.pairs).to.deep.equal(pairToProof);
+      expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
+    });
+
+    it("should gen multiproof for 2 pairs [0, 1]", async () => {
+      const pairToProof: CommitmentFields[] = [pairs[0], pairs[1]];
+      await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
+
+      const smtMultiProof = await getMultiProof(depositor, pairToProof);
+
+      expect(smtMultiProof.pairs).to.deep.equal(pairToProof);
+      expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
+    });
+
+    it("should gen multiproof for 2 pairs [0, 2]", async () => {
+      const pairToProof: CommitmentFields[] = [pairs[0], pairs[2]];
+      await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
+
+      const smtMultiProof = await getMultiProof(depositor, pairToProof);
+
+      expect(smtMultiProof.pairs).to.deep.equal(pairToProof);
+      expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
+    });
+
+    it("should gen multiproof for 2 pairs [1, 2]", async () => {
+      const pairToProof: CommitmentFields[] = [pairs[1], pairs[2]];
+      await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
+
+      const smtMultiProof = await getMultiProof(depositor, pairToProof);
+
+      expect(smtMultiProof.pairs).to.deep.equal(pairToProof);
+      expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
+    });
+
+    it("should gen multiproof for 3 pairs [0, 1, 2]", async () => {
+      const pairToProof: CommitmentFields[] = [pairs[0], pairs[1], pairs[2]];
+      await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
+
+      const smtMultiProof = await getMultiProof(depositor, pairToProof);
+
+      expect(smtMultiProof.pairs).to.deep.equal(pairToProof);
+      expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
+    });
   })
 
   // todo
@@ -469,24 +538,29 @@ describe.only("MultiProof", () => {
     });
   })
 
-  it("should test for gen test cases", async () => {
-    const pairs: CommitmentFields[] =
-      [
-        {
-          secret: '0x00000000551e9caba346265ab896e145a3d3d6359dad756b2b998a5fe1e82b6b',
-          nullifier: '0x000000008660e62918c45569bfc87a180019d029f8257b662d8797f0a4b2e443'
-        },
-        {
-          secret: '0x000000001a51692da01c96034e670c471bffa3899e3b452ac6669abdc57dffe4',
-          nullifier: '0x00000000aa3eb6b01d78d2dc512621301a62ca845066703b3396e94d32659797'
-        }
-      ];
+  it.only("should test for gen test cases", async () => {
+    const pairs: CommitmentFields[] = [
+      {
+        secret: '0x00000000f1ce6c627c1dd83599842a08899acb929480d39be8d98b1f10f1c20f',
+        nullifier: '0x00000000bf4ce6f519179b3835a9b81e662c696e46fa376a7dba6544ab6eb8f1'
+      },
+      {
+        secret: '0x000000007cbc88e3ad8ded8749a70eac76735f42b3d16662bde0d15c6c09cf5b',
+        nullifier: '0x0000000011ff27260e1d055a97ae3b69a5dc717fef1718590055d5a75711d697'
+      },
+      {
+        secret: '0x00000000ed1b4bbd5565a99005e07e6f26444fefe26a4e0375bb552a22df2c1f',
+        nullifier: '0x00000000eab2105c1da1be89554f8aff0ae9093de565b67c799129f8768b089a'
+      }
+    ];
+    // const pairs: CommitmentFields[] = Array.from({ length: 3 }, () => generateSecrets());
     console.log("Contract: ");
     await Promise.all(pairs.map(pair => proceedCertainDeposit(pair)));
     console.log("\nTS: ");
+    console.log(pairs);
 
-    const smtMultiProof = await getMultiProof(depositor, [pairs[0], pairs[1]]);
-    // const smtMultiProof = await getMultiProof(depositor, [pairs[0]]);
+    // const smtMultiProof = await getMultiProof(depositor, [pairs[1]]);
+    const smtMultiProof = await getMultiProof(depositor, pairs);
 
     console.log("\nResults: ");
     console.log(BigInt(await depositor.getRoot()) + " root expected");
@@ -529,6 +603,10 @@ describe.only("MultiProof", () => {
       });
     }
 
+    nodeInfos.forEach(node => {
+      console.log(node.siblings);
+    })
+
     while (nodeInfos[0].siblingIndex !== -1) {
       const maxIndex = Math.max(...nodeInfos.map(node => node.siblingIndex));
       const B: bigint[][] = [];
@@ -543,6 +621,8 @@ describe.only("MultiProof", () => {
           } else {
             pairNextNodeHash = [node.currentNodeHash, BigInt(node.siblings[node.siblingIndex])];
           }
+
+          console.log(pairNextNodeHash);
 
           const elementExists = B.some(existingElement => existingElement[0] === pairNextNodeHash[0] && existingElement[1] === pairNextNodeHash[1]);
 
