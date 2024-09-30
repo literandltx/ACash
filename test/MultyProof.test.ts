1118da9ffa3ce336b2a1f5bdf9f6aede24f3e9b9
literandltx <romanskovronn@gmail.com>
2024-09-30 15:16:29 +0300

    Refactor multiproof generator as helper function

--- test/MultyProof.test.ts
+++ test/MultyProof.test.ts
@@ -1,16 +1,15 @@
 import {
   CommitmentFields,
   generateSecrets,
-  getBytes32PoseidonHash,
   getCommitment,
   getPoseidon,
+  getMultiProof,
   Reverter
 } from "@test-helpers";
 
 import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
 import { Depositor } from "@ethers-v6";
 import { ethers } from "hardhat";
-import { Poseidon } from "@iden3/js-crypto";
 import { expect } from "chai";
 
 const treeHeight = 32; // default
@@ -589,85 +588,6 @@ describe.only("MultiProof", () => {
     expect(smtMultiProof.root).to.be.equal(await depositor.getRoot());
   });
 
-  async function getMultiProof(contract: Depositor, pairs: CommitmentFields[]): Promise<{
-    pairs: CommitmentFields[],
-    siblings: bigint[],
-    root: bigint
-  }> {
-    type NodeInfo = {
-      currentNodeHash: bigint
-      currentNodeKey: bigint
-      siblings: string[]
-      siblingIndex: number
-    };
-    let nodeInfos: NodeInfo[] = [];
-    const M: Set<bigint> = new Set<bigint>();
-
-    const middleHash = (left: bigint, right: bigint) => Poseidon.hash([left, right]);
-    const leafHash = (left: bigint, right: bigint) => Poseidon.hash([left, right, 1n]);
-
-    for (let i = 0; i < pairs.length; i++) {
-      const value: bigint = Poseidon.hash([BigInt(pairs[i].secret), BigInt(pairs[i].nullifier)]);
-      const key: bigint = Poseidon.hash([value]);
-      const smtp = await contract.getProof(ethers.toBeHex(key, 32) as any);
-
-      nodeInfos.push({
-        currentNodeHash: leafHash(key, value),
-        currentNodeKey: key,
-        siblings: smtp.siblings,
-        siblingIndex: findDeepestNonZeroPosition(smtp.siblings)
-      });
-    }
-
-    while (nodeInfos.length !== 0 && nodeInfos[0].siblingIndex !== -1) {
-      const maxIndex = Math.max(...nodeInfos.map(node => node.siblingIndex));
-      const B: bigint[][] = [];
-      const A: bigint[] = [];
-
-      nodeInfos
-        .filter(node => node.siblingIndex === maxIndex)
-        .forEach(node => {
-            const pairNextNodeHash: bigint[] = isRight(node.currentNodeKey, node.siblingIndex) ?
-              [BigInt(node.siblings[node.siblingIndex]), node.currentNodeHash] : [node.currentNodeHash, BigInt(node.siblings[node.siblingIndex])];
-
-            A.push(node.currentNodeHash);
-            B.push(pairNextNodeHash);
-
-            node.currentNodeHash = middleHash(pairNextNodeHash[0], pairNextNodeHash[1]);
-            node.siblingIndex--;
-          }
-        );
-
-      [...new Set(B.flat())]
-        .filter(item => !Array.from(A).includes(BigInt(item)))
-        .forEach(item => M.add(item));
-
-      nodeInfos = distinctNodeInfos(nodeInfos);
-
-      A.length = 0;
-      B.length = 0;
-    }
-
-    return {
-      pairs: pairs,
-      siblings: [...M],
-      root: nodeInfos[0].currentNodeHash
-    }
-  }
-
-  function isRight(value: BigInt, currentDepth: number): boolean {
-    return ((value >> BigInt(currentDepth)) & BigInt(1)) === BigInt(1);
-  }
-
-  function findDeepestNonZeroPosition(array: string[]): number {
-    for (let i = array.length - 1; i >= 0; i--) {
-      if (array[i] !== '0x0000000000000000000000000000000000000000000000000000000000000000') {
-        return i;
-      }
-    }
-
-    return 0;
-  }
 
   async function proceedCertainDeposit(pair: CommitmentFields): Promise<void> {
     await depositor.deposit(getCommitment(pair) as any, { value: ethers.parseEther(eth_value) } as any);
@@ -680,17 +600,5 @@ describe.only("MultiProof", () => {
 
     const shuffled = arr.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, count);
-  }
-
-  function distinctNodeInfos<T extends { currentNodeHash: bigint }>(nodeInfos: T[]): T[] {
-    const uniqueMap: Map<bigint, T> = new Map();
-
-    for (const nodeInfo of nodeInfos) {
-      if (!uniqueMap.has(nodeInfo.currentNodeHash)) {
-        uniqueMap.set(nodeInfo.currentNodeHash, nodeInfo);
-      }
-    }
-
-    return Array.from(uniqueMap.values());
   }
 });
